47e9620749f6c6f3b06fb9944512404a9e3b02d9
kartik vashistha <kartik.vashistha08@gmail.com>
2021-12-09 08:07:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Search from './Search';
+
+
+
+
+
 
 function App() {
+
+  const [input, setInput] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+   {input && <Search/>}
+  
+
+    <button onClick={() =>setInput(!input)}>click me</button>
+
+
     </div>
   );
 }
